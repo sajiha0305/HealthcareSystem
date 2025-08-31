92b350d3a9ba31d60f49a96804f4b4391bde4401
sajiha <sajiha2002@gmail.com>
2025-08-31 16:56:13 +0530

    projectSetup

---
+++ MediLink/src/main/java/com/example/MediLink/MediLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.MediLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediLinkApplication.class, args);
+	}
+
+}
---
+++ MediLink/src/test/java/com/example/MediLink/MediLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MediLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
